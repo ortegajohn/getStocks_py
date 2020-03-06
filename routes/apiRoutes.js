fd1f6fb8181571e46099275548bf87e63053c054
ortegajohn <johnortega@bellsouth.net>
2020-03-05 22:06:57 -0500

    upload all

---
+++ routes/apiRoutes.js
@@ -0,0 +1,105 @@
+var request = require('request');
+var sequelize = require("sequelize");
+require('dotenv').config();
+var apiKey = process.env.comptoken;
+
+var db = require("../models");
+
+module.exports = function (app) {
+  // Get all examples
+  app.get("/api/examples", function (req, res) {
+    // console.log("Keys req.user: ", Object.keys(req.user));
+    // console.log("req.user: ", req.user.id);
+    console.log("ZZZZZZZZZ: ");
+    db.Stocks.findAll({
+      where: sequelize.where(
+        sequelize.literal('user_id'),
+        '=',
+        req.user.id
+      )
+
+    }).then(function (dbStocks) {
+      console.log(dbStocks);
+      console.log("ZZZZZZZZZ: ");
+      res.json(dbStocks);
+    });
+  });
+
+  // Create a new example
+  app.post("/api/examples", function (req, res1) {
+    var getURL = 'https://www.worldtradingdata.com/api/v1/stock?symbol=' + req.body.ticker + '&api_token=' + apiKey;
+
+    string_ticker = req.body.ticker
+
+    console.log("AAAAAAAAAAAAAAAA")
+    var myJSON = JSON.stringify({ticker: string_ticker});
+    request.post({url: 'http://127.0.0.1:5000/companies', json: myJSON});
+    console.log("req:", req.body)
+    console.log("AAAAAAAAAAAAAAAA")
+
+
+    if (req.user !== undefined) {
+      request(getURL, { json: true }, (err, res, body) => {
+        if (err) { return console.log(err); }
+        var test = {
+          ticker: req.body.ticker,
+          name: body.data[0].name,
+          price: parseFloat(body.data[0].price),
+          open: parseFloat(body.data[0].price_open),
+          user_id: parseInt(req.body.user_id),
+          percentChange: parseFloat(body.data[0].change_pct),
+          dayHigh: parseFloat(body.data[0].day_high),
+          dayLow: parseFloat(body.data[0].day_low),
+          marketCap: parseFloat(body.data[0].market_cap)
+        };
+
+
+        db.Stocks.create(test).then(function (dbStocks) {
+
+          console.log("res1.json(test)")
+          res1.json(test);
+
+        });
+
+      });
+    } else {
+
+      request(getURL, { json: true }, (err, res, body) => {
+        if (err) { return console.log(err); }
+        console.log("QQQQQ: ", body)
+
+        var test = {
+          ticker: req.body.ticker,
+          name: body.data[0].name,
+          price: parseFloat(body.data[0].price),
+          open: parseFloat(body.data[0].price_open),
+          user_id: parseInt(req.body.user_id),
+          percentChange: parseFloat(body.data[0].change_pct),
+          dayHigh: parseFloat(body.data[0].day_high),
+          dayLow: parseFloat(body.data[0].day_low),
+          marketCap: parseFloat(body.data[0].market_cap)
+        };
+
+        // console.log("This is var test: " + JSON.stringify(test));
+        res1.redirect('/');
+
+        // db.Stocks.create(test).then(function (dbStocks) {
+        
+        //   res1.redirect('/');
+        // });
+
+      });
+    }
+
+
+  });
+
+  // Delete an example by id
+  app.delete("/api/examples/:id", function (req, res) {
+    db.Stocks.destroy({ where: { id: req.params.id } 
+    }).then(function (req1,res1) {
+      console.log("DELETE Object.keys(dbStocks): ", Object.keys(req1));
+      res.json(req1);
+    });
+  });
+};
---
+++ routes/htmlRoutes.js
@@ -0,0 +1,143 @@
+var db = require("../models");
+var sequelize = require("sequelize");
+var request = require('request');
+
+module.exports = function(app) {
+  // // hmtl route to display homepage (index.html)
+  // app.get("/", function(req, res) {
+  //   res.render("index");
+  // });
+
+  // Load index page
+  // app.get("/", function(req, res) {
+  //   db.Example.findAll({}).then(function(dbExamples) {
+  //     res.render("index", {
+  //       msg: "Welcome!",
+  //       examples: dbExamples
+  //     });
+  //   });
+  // });
+
+  
+  app.get("/", function(req, res2) {
+    // console.log(req)
+    // console.log("redirected here!")
+    // console.log("Object.keys(req): ", Object.keys(req));
+  
+    if(req.user !== undefined  ){
+      db.Stocks.findAll({
+
+        where: sequelize.where(
+          sequelize.literal('user_id'),
+          '=',
+          req.user.id
+        )
+      }).then(function(dbStocks) {
+        // var hbsObject = {
+        //   examples: dbStocks
+        // };
+        // console.log("hbsObject: " + JSON.stringify(hbsObject));
+        console.log("Object.keys(dbStocks): ", Object.keys(dbStocks));
+        // console.log("dbStocks: ", dbStocks);
+        //request.post({url: 'http://127.0.0.1:5000/companies', json: myJSON});
+        request('http://127.0.0.1:5000/companies', { json: true }, (err, res, body) => {
+          if (err) { return console.log(err); }
+          console.log("qqqZZqqq: ", body)
+        });
+
+        res.render("index", { stocks: dbStocks });
+        // location.reload();
+        // return res.redirect("/");
+      });
+    }else{
+
+      db.Stocks.findAll({
+        where: {
+          user_id: {
+            $eq: null
+          }
+      }
+      }).then(function(dbStocks) {
+        // var hbsObject = {
+        //   examples: dbStocks
+        // };
+        // console.log("hbsObject: " + JSON.stringify(hbsObject));
+        // send_this = {}
+        send_this = []
+        console.log("Object.keys(dbStocks): ", Object.keys(dbStocks));
+        console.log("dbStocks: ", dbStocks);
+        request('http://127.0.0.1:5000/companies', { json: true }, (err, res, body) => {
+          if (err) { return console.log(err); }
+          console.log("qqqZAZqqq: ", body)
+          var currentDate = Date.now();
+          
+          try{
+
+            for(i = 0; i < body.length; i++){
+              var random = Math.floor((Math.random() * 10000) + 1);
+              var id_of_stock = currentDate.toString() + random.toString()
+              
+              send_this[i] = {
+                price : body[i][0].price,
+                ticker : body[i][0].symbol,
+                id : id_of_stock
+              }
+              
+            }
+
+            // send_this.price = body[0][0].price
+            // send_this.ticker = body[0][0].symbol  
+            // console.log("currentDate: ", currentDate.toString())
+            // send_this.id = currentDate.toString()
+            console.log("send_thisA: ", send_this)
+
+          } catch {
+
+
+          }
+          
+          res2.render("index", { stocks: send_this });
+          // res2.render("index", { stocks: [send_this] });
+          // res2.render("index", { stocks: dbStocks });
+        });
+
+        
+        // res.render("index", { stocks: dbStocks });
+        // location.reload();
+        // return res.redirect("/");
+      });
+    }
+
+   
+  });
+
+  app.get("/stockpage", function(req, res) {
+    console.log("CHHHUUUURRRRRRRP")
+    db.Stocks.findAll({
+
+
+    }).then(function(dbStocks) {
+      // var hbsObject = {
+      //   examples: dbStocks
+      // };
+      // console.log("hbsObject: " + JSON.stringify(hbsObject));
+      // console.log("Test:",dbStocks)
+      
+      // res.render("index", { stocks: dbStocks });
+      // location.reload();
+       res.redirect("/");
+    });
+  });
+
+  // Load example page and pass in an example by id
+  app.get("/example/:id", function(req, res) {
+    db.Stocks.findOne({ where: { id: req.params.id } }).then(function(
+      dbStocks
+    ) {
+      res.render("Stocks", {
+        example: dbStocks
+      });
+    });
+  });
+};
+
